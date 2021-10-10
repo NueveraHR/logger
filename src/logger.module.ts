78776403d648093a54715b5691608ec8daeae47e
wassimoo <12980387+wassimoo@users.noreply.github.com>
2021-10-10 14:59:26 +0300

    Export as global module

--- src/logger.module.ts
+++ src/logger.module.ts
@@ -1,7 +1,8 @@
-import { DynamicModule, Module } from "@nestjs/common";
+import { DynamicModule, Global, Module } from "@nestjs/common";
 import { LoggerService } from ".";
 import { LoggerOptions } from "./logger.interface";
 
+@Global()
 @Module({})
 export class LoggerModule {
   static forRoot(options: Partial<LoggerOptions>): DynamicModule {
